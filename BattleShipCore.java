7074abc718173de570e1b8c3141a8f28f3e4ad32
Donal O'Shea <renegadethunder@gmail.com>
2013-02-05 14:04:15 +0000

    The first file of the project as a placeholder no code added yet

---
+++ BattleShipCore.java
@@ -0,0 +1,15 @@
+/***********************
+ * PROJECT:       BattleShips
+ * GROUP:         Team Hellfire Zombie Apocalypse of Death and Blood in Your Asshole
+ * 
+ * DESCRIPTION:   We'll add this when it's done
+ *
+ **********************/
+
+
+
+
+
+public class BattleShipCore {
+  public static void main(String args[]);
+}
